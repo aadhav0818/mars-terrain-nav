a3734d105258b6b80f6f366df84cb11ee99a43a4
Aadhav Ilangovan <78950532+aadhav0818@users.noreply.github.com>
2026-09-08 18:16:30 -0500

    Implemented RANSAC and PnP

--- src/terrain_rel_nav.py
+++ src/terrain_rel_nav.py
@@ -72,7 +72,7 @@ fig3, ax3 = plt.subplots()
 # Camera simulation with HiRISE imagery instead of synthetic perlin terrain
 
 images = []
-camera_pos = np.array([[1000, 200, -1000], [1000, 400, -1000], [1000, 600, -1000], [1000, 800, -1000], [1000, 1000, -1000], [1000, 1200, -1000], [1000, 1400, -1000]])
+camera_pos = np.array([[1000, 1200, -1000]])
 
 for pos in tq.tqdm(camera_pos):
     x_intersection, y_intersection, collided_rays = simulate_camera(crop_DTM, pos, pixel_size)
@@ -92,7 +92,7 @@ animation = FuncAnimation(fig3, update, frames=len(images), interval=500)
 # FEATURE DETECTION
 
 reference_image = (illumination * 255).astype(np.uint8)
-camera_image = (images[-2] * 255).astype(np.uint8)
+camera_image = (images[0] * 255).astype(np.uint8)
 sift = cv2.SIFT_create()
 
 reference_keypoints, reference_descriptors = sift.detectAndCompute(reference_image, None)
@@ -110,27 +110,12 @@ for m,n in matches:
 
 # RANSAC
 
-src_pts = np.float32([
-    camera_keypoints[m.queryIdx].pt
-    for m in good_matches
-]).reshape(-1, 1, 2)
+src_pts = np.float32([camera_keypoints[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+dst_pts = np.float32([reference_keypoints[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
-dst_pts = np.float32([
-    reference_keypoints[m.trainIdx].pt
-    for m in good_matches
-]).reshape(-1, 1, 2)
+H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
-H, mask = cv2.findHomography(
-    src_pts,
-    dst_pts,
-    cv2.RANSAC,
-    5.0
-)
-
-inlier_matches = [
-    m for m, inlier in zip(good_matches, mask.ravel())
-    if inlier
-]
+inlier_matches = [m for m, inlier in zip(good_matches, mask.ravel()) if inlier]
 
 print("Good matches:", len(good_matches))
 print("RANSAC inliers:", len(inlier_matches))
@@ -147,16 +132,82 @@ match_image = cv2.drawMatches(
 
 # vertical line separating the two images
 divider_x = camera_image.shape[1]
-cv2.line(
-    match_image,
-    (divider_x, 0),
-    (divider_x, match_image.shape[0]),
-    (255, 255, 255),
-    3
-)
+cv2.line(match_image, (divider_x, 0), (divider_x, match_image.shape[0]), (255, 255, 255), 3)
 
 fig5, ax5 = plt.subplots()
 ax5.imshow(match_image)
 ax5.axis("off")
 
+terrain_points = []
+
+for m in inlier_matches:
+
+    # Reference image pixel coordinates
+    u_ref, v_ref = reference_keypoints[m.trainIdx].pt
+
+    # Convert reference pixel to DTM indices
+    x_index = int(u_ref)
+    y_index = int(v_ref)
+
+    # Convert DTM indices to local terrain coordinates
+    x = x_index * pixel_size
+    y = (crop_height - 1 - y_index) * pixel_size
+    z = crop_DTM[y_index, x_index]
+
+    terrain_points.append([x, y, z])
+
+terrain_points = np.array(terrain_points)
+
+print("Terrain points:", terrain_points.shape)
+print("First 5:")
+print(terrain_points[:5])
+
+# 2D camera points corresponding to the 3D terrain points
+camera_points = np.float32([camera_keypoints[m.queryIdx].pt for m in inlier_matches])
+
+terrain_points = np.float32(terrain_points)
+
+# Input parameters
+resolution_width = 1280
+resolution_height = 780
+Hfov_degrees = 60
+
+Hfov = np.deg2rad(Hfov_degrees)
+Vfov = 2*np.arctan(np.tan(Hfov/2) * (resolution_height/resolution_width))
+fx = (resolution_width / 2) / np.tan(Hfov / 2)
+fy = (resolution_height / 2) / np.tan(Vfov / 2)
+cx = resolution_width / 2
+cy = resolution_height / 2
+
+# Camera intrinsic matrix
+K = np.array([
+    [fx, 0, cx],
+    [0, fy, cy],
+    [0, 0, 1]
+], dtype=np.float64)
+
+# No lens distortion (for now)
+dist_coeffs = np.zeros((4, 1))
+
+success, rvec, tvec, inliers = cv2.solvePnPRansac(
+    terrain_points,
+    camera_points,
+    K,
+    dist_coeffs,
+    flags=cv2.SOLVEPNP_ITERATIVE
+)
+
+print("PnP success:", success)
+print("PnP inliers:", len(inliers))
+print("rvec:", rvec.ravel())
+print("tvec:", tvec.ravel())
+
+R_camera, _ = cv2.Rodrigues(rvec)
+
+camera_position_estimated = -R_camera.T @ tvec
+
+print("Estimated camera position:")
+print(camera_position_estimated.ravel())
+print("Position error:")
+print(np.linalg.norm(camera_position_estimated.ravel()-camera_pos[0]))
 plt.show()
